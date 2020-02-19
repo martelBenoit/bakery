232eee3371ce2dcb394dc7fbc86efcdd2f9180fb
Tolrod22 <loic.travaille@gmail.com>
2020-02-19 16:59:14 +0100

    Changing payment port

--- payments/src/main/java/fr/ensibs/service/PaymentsManagerPublisher.java
+++ payments/src/main/java/fr/ensibs/service/PaymentsManagerPublisher.java
@@ -4,6 +4,6 @@ import javax.xml.ws.Endpoint;
 
 public class PaymentsManagerPublisher {
     public static void main(String[] args) {
-        Endpoint.publish("http://localhost:9991/ws/paymentssmanagementservice", new PaymentsManagementServiceImpl());
+        Endpoint.publish("http://localhost:9992/ws/paymentssmanagementservice", new PaymentsManagementServiceImpl());
     }
 }
